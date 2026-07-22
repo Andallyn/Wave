612fd10d5452c8c537ea9d5fe912589638091dc5
Andallyn <148128391+Andallyn@users.noreply.github.com>
2026-07-22 22:08:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Public browser configuration. The Supabase anon key is designed to be public and is protected by Row Level Security.
 // Never place a Supabase service-role key or any other privileged secret in this file.
 window.WAVE_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://rnwsxjzmtqcanpfazpsa.supabase.co'
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJud3N4anptdHFjYW5wZmF6cHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3NDg1MjMsImV4cCI6MjEwMDMyNDUyM30.AbbFaitLhC3DI4PI8UFekcd_VPNimWklM2t3rNjk6Tw' 
 };
